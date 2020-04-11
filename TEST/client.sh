44103765de3c58ff7b84eaccaf3a1059a0501e2e
Wenjie Bai <bai104@purdue.edu>
2020-04-10 21:44:59 -0400

    sh

---
+++ TEST/client.sh
@@ -0,0 +1,3 @@
+sudo ip addr add 10.0.2.1/24 dev tun0
+sudo ifconfig tun0 up
+sudo route add -net 10.0.1.0 netmask 255.255.255.0 dev tun0
--- tls/client.sh
+++
@@ -1,3 +0,0 @@
-sudo ifconfig tun0 192.168.53.5/24 up
-sudo route add -net 192.168.60.0/24
-
